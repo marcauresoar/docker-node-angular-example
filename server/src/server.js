939a0acba6911f03c70e602648e702ea16989243
Marco Aurelio Soares <maurelio@avenuecode.com>
2017-11-27 13:10:55 -0200

    starting to create accessories application

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const accessories = require('./accessories.js');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -26,8 +29,37 @@ app.use(function (req, res, next) {
     next();
 });
 
+app.use(fileUpload());
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
+
   res.send('{ "data": "Hello World!" }');
+});
+
+app.post('/upload', function(req, res) {
+  if (!req.files || !req.files.inputFile)
+    return res.status(400).send('No files were uploaded.');
+
+  // The name of the input field (i.e. "inputFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.inputFile;
+
+  let fileName = __dirname + '/uploads/input.csv';
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(fileName, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    accessories.processAccessories(fileName, (dataResult) => {
+      res.status(200).send(dataResult);
+    });
+  });
 });
 
 app.listen(PORT, HOST);
